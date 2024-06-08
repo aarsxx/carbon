43a052b0328517b0ee4a77048a7badc5bb85c141
andikaleonardo <leonardodika.13@gmail.com>
2024-06-08 11:37:22 +0700

    fix: fix lint @about.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -14,7 +14,7 @@ export default function About() {
     return (
         <motion.section
             ref={ref}
-            className="mb-50 max-w-[45rem] text-start leading-8 sm:mb-40 scroll-mt-28 mb-28 "
+            className="mb-50 max-w-[45rem] text-start leading-8 sm:mb-40 scroll-mt-28 mb-28"
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.175 }}
@@ -29,11 +29,10 @@ export default function About() {
                         My journey into programming started during my undergrad at Bina Nusantara University. Through internships, I gained hands-on experience in iOS development, sparking my passion for creating impactful technology. Now, as a Software Engineer at Epidemic Sound, I focus on implementing algorithms for digital signal processing using Django and Golang.
                     </p>
                     <p className="mb-3">
-                        I specialize in technologies like React, React Native, Django, and Golang, and I'm fluent in Bahasa, English, and proficient in Chinese. Beyond work, I was also recognized at the Apple WWDC Swift Student Challenge. I enjoy sharing my tech discoveries on GitHub and YouTube and unwinding with cooking, movies, and workouts.
+                        I specialize in technologies like React, React Native, Django, and Golang, and I&apos;m fluent in Bahasa, English, and proficient in Chinese. Beyond work, I was also recognized at the Apple WWDC Swift Student Challenge. I enjoy sharing my tech discoveries on GitHub and YouTube and unwinding with cooking, movies, and workouts.
                     </p>
                 </>
             )}
         </motion.section>
     )
 }
-
