7e6b429414efc8ddf2c883d7ae752b67db6c82c4
andikaleonardo <leonardodika.13@gmail.com>
2024-06-13 23:25:26 +0700

    fix: error tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["var(--font-open-sans)", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+};
