f177b36a741d795f207e20ff6b0f8d916dc0d910
andikaleonardo <leonardodika.13@gmail.com>
2024-06-23 14:38:47 +0700

    feat: add project page

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,28 +3,31 @@ export type Project = {
   techs: string[];
   link: string;
   isComingSoon?: boolean;
+  selected?: boolean;  // New property
 };
-
 const projects: Project[] = [
   {
     title: "One",
-    techs: ["Rust","Postgres","Redis","Rabbitmq","Docker"],
+    techs: ["rust","postgres","redis","rabbitmq","docker"],
     link: "https://github.com/aarsxx/one",
     isComingSoon: true,
+    selected: true,
   },
   {
     title: "Cosmos",
-    techs: ["Astro","Tailwindcss","Typescript"],
+    techs: ["astro","tailwindcss","typescript"],
     link: "https://github.com/aarsxx/atom",
+    selected: true,
   },
   {
     title: "Dotfiles",
-    techs: ["Shell"],
+    techs: ["shell"],
     link: "https://github.com/aarsxx/dotfiles",
+    selected: true,
   },
   {
     title: "Brave iOS",
-    techs: ["Swift"],
+    techs: ["swift"],
     link: "https://github.com/brave/brave-ios",
   },
   {
