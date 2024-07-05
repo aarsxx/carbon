227f23635ae0d92cab8e974d4e69c50ace795c8e
andikaleonardo <leonardodika.13@gmail.com>
2024-07-05 09:02:19 +0700

    feat: add pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,9 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
-import { SITE_URL } from "./src/data/config";
-
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), sitemap(), robotsTxt()],
-    site: SITE_URL,
-    markdown: {
-        syntaxHighlight: "shiki",
-        shikiConfig: {
-            theme: "nord",
-            wrap: false
-        }
-    }
+    site: "https://cosmos.arsx.xyz",
+    integrations: [react(), tailwind()],
 });
